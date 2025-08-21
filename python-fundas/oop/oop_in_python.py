1ce25673d9350aa9f42b4b1f2ab80d225c77c55f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-21 14:41:28 +0530

    initial commit

---
+++ python-fundas/oop/oop_in_python.py
@@ -0,0 +1,36 @@
+# functions and methods 
+# function - outside 
+# method - inside class 
+
+class Employee:
+    
+    # id: int
+    # name: str 
+    # salary: int
+
+    # constructor method __init__
+    def __init__(self, id, name, salary):
+        self.id = id
+        self.name = name
+        self.salary = salary
+
+    def __str__(self):
+        return self.name
+    
+    def get_salary(self):
+        return self.salary
+
+# instance methods, class methods, static methods 
+
+
+    
+emp = Employee(101, "Sonu", 20000)
+# emp = Employee() # no constr 
+print(emp)
+
+
+
+
+
+
+
