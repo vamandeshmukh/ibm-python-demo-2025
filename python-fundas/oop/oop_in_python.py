14c4c3b47ed7b077679824d2a164d91aa4eb43d9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-21 16:14:43 +0530

    initial commit

--- python-fundas/oop/oop_in_python.py
+++ python-fundas/oop/oop_in_python.py
@@ -1,117 +1,135 @@
-# # # functions and methods 
-# # # function - outside 
-# # # method - inside class 
+# # # # functions and methods 
+# # # # function - outside 
+# # # # method - inside class 
 
-# # class Person: 
-# #     def _init_(self):
-# #         pass
+# # # class Person: 
+# # #     def _init_(self):
+# # #         pass
 
-# #     def speak(self):
-# #         print('speaking...')
+# # #     def speak(self):
+# # #         print('speaking...')
 
-# # class Employee (Person):
+# # # class Employee (Person):
     
-# #     # id: int
-# #     # name: str 
-# #     # salary: int
+# # #     # id: int
+# # #     # name: str 
+# # #     # salary: int
 
-# #     # constructor method __init__
-# #     def __init__(self, id, name, salary):
-# #         self.id = id
-# #         self.name = name
-# #         self.salary = salary
+# # #     # constructor method __init__
+# # #     def __init__(self, id, name, salary):
+# # #         self.id = id
+# # #         self.name = name
+# # #         self.salary = salary
 
-# #     # def __str__(self):
-# #     #     return self.name
+# # #     # def __str__(self):
+# # #     #     return self.name
     
-# #     def get_salary(self):
-# #         return self.salary
+# # #     def get_salary(self):
+# # #         return self.salary
 
-# # # instance methods, class methods, static methods 
+# # # # instance methods, class methods, static methods 
 
-# #     @classmethod
-# #     def get_data(cls):
-# #         pass
+# # #     @classmethod
+# # #     def get_data(cls):
+# # #         pass
 
-# #     @classmethod
-# #     def get_more_data(cls):
-# #         pass
+# # #     @classmethod
+# # #     def get_more_data(cls):
+# # #         pass
 
-# #     @staticmethod
-# #     def get_yet_more_data():
-# #         pass
+# # #     @staticmethod
+# # #     def get_yet_more_data():
+# # #         pass
 
-# # emp = Employee(101, "Sonu", 20000)
-# # # emp = Employee() # no constr 
-# # print(emp)
-# # emp2 = Employee(102, "Monu", 3000)
-# # Employee.get_data()
+# # # emp = Employee(101, "Sonu", 20000)
+# # # # emp = Employee() # no constr 
+# # # print(emp)
+# # # emp2 = Employee(102, "Monu", 3000)
+# # # Employee.get_data()
 
-# # emp.speak()
+# # # emp.speak()
 
-# # # employee = {}
+# # # # employee = {}
 
-# # # def employee_Service():
-# # #     pass  
+# # # # def employee_Service():
+# # # #     pass  
 
 
 
-# class Person:
+# # class Person:
 
-#     # def __new__(cls):
-#     #     pass 
+# #     # def __new__(cls):
+# #     #     pass 
+
+# #     def __init__(self):
+# #         print("person constructor called")
+
+# # class Employee (Person): 
+
+# #     def __init__(self):
+# #         print("employee constructor called")
+
+# # # per = Person()
+# # emp = Employee()
+
+# # # methods with __ and _ 
+# # # private protected public in Python 
+# # # public = method /variable without any underescore 
+# # # protected  = method /variable with single underescore 
+# # # private  = method /variable with double underescore 
+# # @property
+# # @dataclass
+
+# class Person: 
 
 #     def __init__(self):
-#         print("person constructor called")
+#         print("Person constructor")
 
-# class Employee (Person): 
+#     def speak(self):
+#         print('speaking...')
 
+#     def _person_protected(self):
+#         print("person protected method")
+
+#     def __person_private(self):
+#         print("person protected method")
+
+# class Employee (Person):
+    
 #     def __init__(self):
-#         super()
-#         print("employee constructor called")
+#         super().__init__()
+#         print("Employee constructor")
+
+#     def __employee_private(self):
+#         print("employee private method")
+
+#     def _employee_protected(self):
+#         print("employee protected method")
+
+#     @property
+#     def method_property(self):
+#         print("method or property?")
 
 # # per = Person()
+# # per.speak()
+# # # per.__person_private() # error 
+# # per._person_protected() # works but don't call
 # emp = Employee()
+# # emp.speak()
+# # # emp.__employee_private() # error 
+# # # emp.__person_private() # error 
+# # emp._employee_protected  # works but don't call
+# # emp.method_property()
+# emp.method_property
 
-# # methods with __ and _ 
-# # private protected public in Python 
-# # public = method /variable without any underescore 
-# # protected  = method /variable with single underescore 
-# # private  = method /variable with double underescore 
+from  dataclasses import dataclass
 
-
-class Person: 
-
-    def _init_(self):
-        print("Person constructor")
-
-    def speak(self):
-        print('speaking...')
-
-    def _protected_method(self):
-        print("protected method")
-
-    def __private_method(self):
-        print("protected method")
-
-class Employee (Person):
+@dataclass
+class Employee:
     
-    def __init__(self):
-        print("Employee constructor")
+    id: int
+    name: str 
+    salary: int
 
-    def __call_only_here(self):
-        print("private method")
-
-    def _call_only_in_subclass(self):
-        print("protected method")
-
-
-emp = Employee()
-
-# emp.__call_only_here()
-emp._call_only_in_subclass()
-emp._protected_method()
-
-per = Person()
-# per.__private_method()
-per._protected_method()
+emp = Employee(101, "Sonu", 10.50)
+print(emp)
