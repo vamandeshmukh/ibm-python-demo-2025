8b469384f038cbb94d2c5f75b0f749f1d009f003
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-21 15:15:39 +0530

    initial commit

--- python-fundas/oop/oop_in_python.py
+++ python-fundas/oop/oop_in_python.py
@@ -1,47 +1,80 @@
-# functions and methods 
-# function - outside 
-# method - inside class 
+# # functions and methods 
+# # function - outside 
+# # method - inside class 
 
-class Employee:
+# class Person: 
+#     def _init_(self):
+#         pass
+
+#     def speak(self):
+#         print('speaking...')
+
+# class Employee (Person):
     
-    # id: int
-    # name: str 
-    # salary: int
+#     # id: int
+#     # name: str 
+#     # salary: int
 
-    # constructor method __init__
-    def __init__(self, id, name, salary):
-        self.id = id
-        self.name = name
-        self.salary = salary
+#     # constructor method __init__
+#     def __init__(self, id, name, salary):
+#         self.id = id
+#         self.name = name
+#         self.salary = salary
 
-    # def __str__(self):
-    #     return self.name
+#     # def __str__(self):
+#     #     return self.name
     
-    def get_salary(self):
-        return self.salary
+#     def get_salary(self):
+#         return self.salary
 
-# instance methods, class methods, static methods 
+# # instance methods, class methods, static methods 
 
-    @classmethod
-    def get_data():
-        pass
+#     @classmethod
+#     def get_data(cls):
+#         pass
 
-    # @instancemethod
-    def get_more_data():
-        pass
+#     @classmethod
+#     def get_more_data(cls):
+#         pass
 
-emp = Employee(101, "Sonu", 20000)
-# emp = Employee() # no constr 
-print(emp)
+#     @staticmethod
+#     def get_yet_more_data():
+#         pass
+
+# emp = Employee(101, "Sonu", 20000)
+# # emp = Employee() # no constr 
+# print(emp)
+# emp2 = Employee(102, "Monu", 3000)
+# Employee.get_data()
+
+# emp.speak()
+
+# # employee = {}
+
+# # def employee_Service():
+# #     pass  
+
+
+
+class Person:
+
+    # def __new__(cls):
+    #     pass 
+
+    def __init__(self):
+        print("person constructor called")
+
+class Employee (Person): 
+
+    def __init__(self):
+        super()
+        print("employee constructor called")
+
+# per = Person()
+emp = Employee()
 
 
 
 
-
-employee = {}
-
-
-
-def employee_Service():
-    pass  
+# methods with __ and _ 
 
