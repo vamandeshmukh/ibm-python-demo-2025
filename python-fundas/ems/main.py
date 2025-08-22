1aec7546fb65e093f2fbb4461c52fa0924e516ba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-22 15:17:10 +0530

    modules and packages

--- python-fundas/ems/main.py
+++ python-fundas/ems/main.py
@@ -1,7 +1,17 @@
 # main.py
 
+import sys
+
+sys.path.append(".")
+
+
+from model.Employee import Employee
 from calc_stuff import add_nums
+
 
 print("main")
 sum = add_nums(10, 20)
 print(sum)
+
+emp = Employee(101, "Sonu", 10.50)
+print(emp)
---
+++ python-fundas/ems/model/Employee.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Employee:
+
+    id: int
+    name: str
+    salary: float
