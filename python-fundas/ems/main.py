cdfadb2c81a49fd7f775e732c863a1777e3b8b55
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-22 16:15:18 +0530

    python

--- python-fundas/ems/main.py
+++ python-fundas/ems/main.py
@@ -1,17 +1,14 @@
-# main.py
+# # main.py
 
-import sys
-
-sys.path.append(".")
-
-
-from model.Employee import Employee
-from calc_stuff import add_nums
+# from model.Employee import Employee
+# from calc_stuff import add_nums
 
 
-print("main")
-sum = add_nums(10, 20)
-print(sum)
+# print("main")
+# sum = add_nums(10, 20)
+# print(sum)
 
-emp = Employee(101, "Sonu", 10.50)
-print(emp)
+# emp = Employee(101, "Sonu", 10.50)
+# print(emp)
+
+
