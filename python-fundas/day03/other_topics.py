0de1c02cf7d5ccdae9c8575e8cdad27c0723eefb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-25 10:03:12 +0530

    generators and exceptions

--- python-fundas/day03/other_topics.py
+++ python-fundas/day03/other_topics.py
@@ -1,29 +1,28 @@
-# Generators in Python and Exception handling
+# Advanced Python Modules
+
+import numpy as np
+import pandas as pd
 
 
-def gen(n):
-    i = 1
-    while i <= n:
-        yield i  # next
-        i += 1
+data = {
+    "id": [101, 102, 103],
+    "name": ["Sonu", "Monu", "Tonu"],
+    "salary": [50000, 55000, 48000],
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df["salary"].mean())
 
 
-val = gen(5)
+# marks = np.array([98, 95, 99, 95, 97])
 
-while True:
-    try:
-        print(next(val))
-    except StopIteration:
-        print("done")
-        break
-    except Exception:
-        print("done")
-        break
-    finally:
-        print("closing...")
+# print(marks)
+# print(np.max(marks))
+# print(np.sort(marks))
 
 
-# # Generators in Python
+# # Generators in Python and Exception handling
 
 
 # def gen(n):
@@ -34,12 +33,37 @@ while True:
 
 
 # val = gen(5)
-# print(next(val))
-# print(next(val))
-# print(next(val))
-# print(next(val))
-# print(next(val))
-# print(next(val))
 
-# # for num in gen(5):
-# #     print(num)
+# while True:
+#     try:
+#         print(next(val))
+#     except StopIteration:
+#         print("done")
+#         break
+#     except Exception:
+#         print("done")
+#         break
+#     finally:
+#         print("closing...")
+
+
+# # # Generators in Python
+
+
+# # def gen(n):
+# #     i = 1
+# #     while i <= n:
+# #         yield i  # next
+# #         i += 1
+
+
+# # val = gen(5)
+# # print(next(val))
+# # print(next(val))
+# # print(next(val))
+# # print(next(val))
+# # print(next(val))
+# # print(next(val))
+
+# # # for num in gen(5):
+# # #     print(num)
