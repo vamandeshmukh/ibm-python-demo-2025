f0c350be5966ee61d5d9032947ac6fb4d430ff80
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-25 10:12:57 +0530

    generators and exceptions

--- python-fundas/day03/other_topics.py
+++ python-fundas/day03/other_topics.py
@@ -1,53 +1,38 @@
-# Advanced Python Modules
+# # Advanced Python Modules
 
-import numpy as np
-import pandas as pd
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://localhost:27017/")
+db = client["ems"]
+employees = db["employees"]
 
-data = {
-    "id": [101, 102, 103],
-    "name": ["Sonu", "Monu", "Tonu"],
-    "salary": [50000, 55000, 48000],
-}
+emp = employees.find_one({"first_name": "Sonu"})
+print(emp)
+# employees.
 
-df = pd.DataFrame(data)
-print(df)
-print(df["salary"].mean())
-
-
-# marks = np.array([98, 95, 99, 95, 97])
-
-# print(marks)
-# print(np.max(marks))
-# print(np.sort(marks))
+# import numpy as np
+# import pandas as pd
 
 
-# # Generators in Python and Exception handling
+# data = {
+#     "id": [101, 102, 103],
+#     "name": ["Sonu", "Monu", "Tonu"],
+#     "salary": [50000, 55000, 48000],
+# }
+
+# df = pd.DataFrame(data)
+# print(df)
+# print(df["salary"].mean())
 
 
-# def gen(n):
-#     i = 1
-#     while i <= n:
-#         yield i  # next
-#         i += 1
+# # marks = np.array([98, 95, 99, 95, 97])
+
+# # print(marks)
+# # print(np.max(marks))
+# # print(np.sort(marks))
 
 
-# val = gen(5)
-
-# while True:
-#     try:
-#         print(next(val))
-#     except StopIteration:
-#         print("done")
-#         break
-#     except Exception:
-#         print("done")
-#         break
-#     finally:
-#         print("closing...")
-
-
-# # # Generators in Python
+# # # Generators in Python and Exception handling
 
 
 # # def gen(n):
@@ -58,12 +43,37 @@ print(df["salary"].mean())
 
 
 # # val = gen(5)
-# # print(next(val))
-# # print(next(val))
-# # print(next(val))
-# # print(next(val))
-# # print(next(val))
-# # print(next(val))
 
-# # # for num in gen(5):
-# # #     print(num)
+# # while True:
+# #     try:
+# #         print(next(val))
+# #     except StopIteration:
+# #         print("done")
+# #         break
+# #     except Exception:
+# #         print("done")
+# #         break
+# #     finally:
+# #         print("closing...")
+
+
+# # # # Generators in Python
+
+
+# # # def gen(n):
+# # #     i = 1
+# # #     while i <= n:
+# # #         yield i  # next
+# # #         i += 1
+
+
+# # # val = gen(5)
+# # # print(next(val))
+# # # print(next(val))
+# # # print(next(val))
+# # # print(next(val))
+# # # print(next(val))
+# # # print(next(val))
+
+# # # # for num in gen(5):
+# # # #     print(num)
