607f055a6a8bd51f6e6da13f700ef688a9413527
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-25 12:07:03 +0530

    generators and exceptions

--- python-fundas/day03/other_topics.py
+++ python-fundas/day03/other_topics.py
@@ -1,90 +1,97 @@
+
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://en.wikipedia.org/wiki/IBM"
-response = requests.get(url)
+def get_coordinates(city):
+    url = f"https://en.wikipedia.org/wiki/{city}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(response.text, "html.parser")
+    lat = soup.find("span", class_="latitude")
+    lon = soup.find("span", class_="longitude")
 
-infobox = soup.find("table", {"class": "infobox"})
+    if lat and lon:
+        return lat.text, lon.text
+    return None, None
 
+def create_maps_url(city1, city2):
+    lat1, lon1 = get_coordinates(city1)
+    lat2, lon2 = get_coordinates(city2)
 
-def get_infobox_value(infobox, label):
-    row = infobox.find("th", string=label)
-    if row:
-        return row.find_next_sibling("td").get_text(strip=True)
-    return None
+    if lat1 and lon1 and lat2 and lon2:
+        url = f"https://www.google.com/maps/dir/{lat1},{lon1}/{lat2},{lon2}"
+        return url
+    else:
+        return "Coordinates not found."
 
-
-founded = get_infobox_value(infobox, "Founded")
-industry = get_infobox_value(infobox, "Industry")
-products = get_infobox_value(infobox, "Products")
-
-print("Founded:", founded)
-print("Industry:", industry)
-print("products:", products)
-
-
-# # # Advanced Python Modules
-
-# from pymongo import MongoClient
-
-# client = MongoClient("mongodb://localhost:27017/")
-# db = client["ems"]
-# employees = db["employees"]
-
-# emp = employees.find_one({"first_name": "Sonu"})
-# print(emp)
-# # employees.
-
-# # import numpy as np
-# # import pandas as pd
+maps_url = create_maps_url("Chennai", "Mumbai")
+print("Google Maps URL:", maps_url)
 
 
-# # data = {
-# #     "id": [101, 102, 103],
-# #     "name": ["Sonu", "Monu", "Tonu"],
-# #     "salary": [50000, 55000, 48000],
-# # }
-
-# # df = pd.DataFrame(data)
-# # print(df)
-# # print(df["salary"].mean())
 
 
-# # # marks = np.array([98, 95, 99, 95, 97])
+# import requests
+# from bs4 import BeautifulSoup
 
-# # # print(marks)
-# # # print(np.max(marks))
-# # # print(np.sort(marks))
+# url = "https://en.wikipedia.org/wiki/IBM"
+# response = requests.get(url)
 
+# soup = BeautifulSoup(response.text, "html.parser")
 
-# # # # Generators in Python and Exception handling
-
-
-# # # def gen(n):
-# # #     i = 1
-# # #     while i <= n:
-# # #         yield i  # next
-# # #         i += 1
+# infobox = soup.find("table", {"class": "infobox"})
 
 
-# # # val = gen(5)
-
-# # # while True:
-# # #     try:
-# # #         print(next(val))
-# # #     except StopIteration:
-# # #         print("done")
-# # #         break
-# # #     except Exception:
-# # #         print("done")
-# # #         break
-# # #     finally:
-# # #         print("closing...")
+# def get_infobox_value(infobox, label):
+#     row = infobox.find("th", string=label)
+#     if row:
+#         return row.find_next_sibling("td").get_text(strip=True)
+#     return None
 
 
-# # # # # Generators in Python
+# founded = get_infobox_value(infobox, "Founded")
+# industry = get_infobox_value(infobox, "Industry")
+# products = get_infobox_value(infobox, "Products")
+
+# print("Founded:", founded)
+# print("Industry:", industry)
+# print("products:", products)
+
+
+# # # # Advanced Python Modules
+
+# # from pymongo import MongoClient
+
+# # client = MongoClient("mongodb://localhost:27017/")
+# # db = client["ems"]
+# # employees = db["employees"]
+
+# # emp = employees.find_one({"first_name": "Sonu"})
+# # print(emp)
+# # # employees.
+
+# # # import numpy as np
+# # # import pandas as pd
+
+
+# # # data = {
+# # #     "id": [101, 102, 103],
+# # #     "name": ["Sonu", "Monu", "Tonu"],
+# # #     "salary": [50000, 55000, 48000],
+# # # }
+
+# # # df = pd.DataFrame(data)
+# # # print(df)
+# # # print(df["salary"].mean())
+
+
+# # # # marks = np.array([98, 95, 99, 95, 97])
+
+# # # # print(marks)
+# # # # print(np.max(marks))
+# # # # print(np.sort(marks))
+
+
+# # # # # Generators in Python and Exception handling
 
 
 # # # # def gen(n):
@@ -95,12 +102,37 @@ print("products:", products)
 
 
 # # # # val = gen(5)
-# # # # print(next(val))
-# # # # print(next(val))
-# # # # print(next(val))
-# # # # print(next(val))
-# # # # print(next(val))
-# # # # print(next(val))
 
-# # # # # for num in gen(5):
-# # # # #     print(num)
+# # # # while True:
+# # # #     try:
+# # # #         print(next(val))
+# # # #     except StopIteration:
+# # # #         print("done")
+# # # #         break
+# # # #     except Exception:
+# # # #         print("done")
+# # # #         break
+# # # #     finally:
+# # # #         print("closing...")
+
+
+# # # # # # Generators in Python
+
+
+# # # # # def gen(n):
+# # # # #     i = 1
+# # # # #     while i <= n:
+# # # # #         yield i  # next
+# # # # #         i += 1
+
+
+# # # # # val = gen(5)
+# # # # # print(next(val))
+# # # # # print(next(val))
+# # # # # print(next(val))
+# # # # # print(next(val))
+# # # # # print(next(val))
+# # # # # print(next(val))
+
+# # # # # # for num in gen(5):
+# # # # # #     print(num)
